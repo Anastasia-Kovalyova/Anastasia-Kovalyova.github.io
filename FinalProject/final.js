9256ef58be45a3ba5538a89caa35aaf1117d9c9a
Anastasia-Kovalyova <kov19003@byui.edu>
2020-04-08 22:12:02 -0600

    finished final project

--- FinalProject/final.js
+++ FinalProject/final.js
@@ -3,10 +3,10 @@ setInterval(() => toggleImage(), 4000);
 let currentIndex = 0;
 
 let images = [
-  "../images/man-and-woman-lying-on-a-bed-of-flowers-2871648.jpg",
-  "../images/man-and-woman-holding-lights-while-kissing-3014859.jpg",
-  "../images/man-and-woman-standing-beside-carriage-3204420.jpg",
-  "../images/grayscale-photo-of-newly-wed-2209416.jpg",
+  "../images/rsz_man-and-woman-lying-on-a-bed-of-flowers-2871648.jpg",
+  "../images/rsz_man-and-woman-holding-lights-while-kissing-3014859.jpg",
+  "../images/rsz_man-and-woman-standing-beside-carriage-3204420.jpg",
+  "../images/rsz_grayscale-photo-of-newly-wed-2209416.jpg",
 ];
 
 function toggleImage() {
